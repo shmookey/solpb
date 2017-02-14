0a8e622d1ea5f25d6040550e1414cf8d9d401a08
Luke Williams <shmookey@shmookey.net>
2017-02-14 13:07:31 +1100

    Initial import

---
+++ src/Main.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import qualified Options.Applicative as Opts
+import qualified Data.ByteString.Lazy as B
+import Options.Applicative ((<>))
+import qualified Text.ProtocolBuffers.ProtoCompile.Parser as Parser
+import qualified Text.DescriptorProtos.FileDescriptorProto as Proto
+
+
+data Options = Options
+  { optInput :: String
+  } deriving (Show)
+
+
+
+readCliOpts :: IO Options
+readCliOpts =
+  Opts.execParser $ Opts.info (Opts.helper <*> cliOpts)
+    ( Opts.fullDesc
+   <> Opts.header   "solpb -- protocol buffers for solidity" 
+   <> Opts.progDesc "Generate solidity libraries for working with protocol buffers." )
+  where 
+    cliOpts = Options
+      <$> Opts.argument Opts.str
+          ( Opts.metavar "FILE"
+         <> Opts.help    "Path to input file." )
+
+main :: IO ()
+main = readCliOpts >>= \o ->
+  let
+    input = optInput o
+  in do
+    result <- Parser.parseProto input <$> B.readFile input
+    putStrLn $ case result of
+      Left e  -> show e
+      Right x -> show x
+
---
+++ src/Solidity.hs
@@ -0,0 +1,145 @@
+module Solidity where
+
+import qualified Data.Map as Map
+import Text.Printf (printf)
+import Data.List (intercalate)
+import Data.Map (Map)
+
+data Type
+  = UInt | UInt32 | UInt64
+  deriving (Eq, Ord)
+
+type FieldName  = String
+type StructName = String
+
+data Field  = Field Int String Type
+data Setter = Setter StructName Type [Field]
+data Struct = Struct StructName [Field]
+
+class Format x where
+  fmt :: x -> String
+
+instance Format Type where
+  fmt x = case x of
+    UInt   -> "uint"
+    UInt32 -> "uint32"
+    UInt64 -> "uint64"
+
+instance Format Setter where
+  fmt (Setter structName setterType fields) =
+    let
+      typ     = fmt setterType
+      name    = "setField_" ++ typ
+      args    = printf "(%s x, uint fieldId, %s obj)" typ structName :: String
+      proto   = printf "function %s%s internal constant" name args :: String
+      ifs     = (intercalate " else " $ map iff fields) ++ term
+      term    = " else {\n  throw;\n}"
+
+      iff :: Field -> String
+      iff (Field i k _) = printf "if (%s) {\n  %s\n}" cond action
+        where cond   = (printf "fieldId == %i" i) :: String
+              action = (printf "obj.%s = x;" k) :: String
+
+    in
+      printf "%s {\n%s}" proto (indent 2 ifs)
+
+instance Format Struct where
+  fmt (Struct name fields) =
+    let
+      fieldLines = indent 2 . intercalate "\n" $ map field fields
+
+      field :: Field -> String
+      field (Field _ k t) = printf "%s %s;" (fmt t) k
+
+    in
+      printf "struct %s {\n%s}" name fieldLines
+
+formatLibrary :: Struct -> String
+formatLibrary x@(Struct name fields) =
+  let
+    decoder = indent 2 $ formatDecoder name
+    statics = indent 2 staticCode
+    setters = indent 2 . intercalate "\n" . map fmt $ createSetters name fields
+    struct  = indent 2 $ fmt x
+  in
+    printf "library %s {\n%s\n%s\n%s\n%s\n}" name struct statics decoder setters
+
+createSetters :: StructName -> [Field] -> [Setter]
+createSetters name = 
+  let
+   accumulate :: Map Type [Field] -> Field -> Map Type [Field]
+   accumulate acc x@(Field i k t) = case Map.lookup t acc of
+     Just xs -> Map.insert t (x:xs) acc
+     Nothing -> Map.insert t [x] acc
+  in
+    map (uncurry $ Setter name) . Map.toList . foldl accumulate mempty
+
+indent :: Int -> String -> String
+indent n = unlines . map (spaces ++) . lines
+  where spaces = take n $ repeat ' '
+
+formatDecoder :: StructName -> String
+formatDecoder t = printf "\
+  \function decode%s(bytes data) internal constant returns (%s) { \n\
+  \  %s buf; \n\
+  \  \n\
+  \  // The size of `data` and its first element are offset by a 256-bit length \n\
+  \  uint len = data.length + 32; \n\
+  \  uint ptr = 32; \n\
+  \  \n\
+  \  while (ptr < len) { \n\
+  \    var (fieldId, wireType, n) = readKey(ptr, data); \n\
+  \    ptr += n; \n\
+  \  \n\
+  \    if(wireType == WireType.Varint) { \n\
+  \      var (x1, sz) = readVarInt(ptr, data); \n\
+  \      ptr += sz; \n\
+  \      setField_uint(fieldId, x1, buf); \n\
+  \  \n\
+  \    } else if(wireType == WireType.Fixed64) { \n\
+  \      uint64 x2 = readUInt64(ptr, data); \n\
+  \      ptr += 8; \n\
+  \      setField_uint64(fieldId, x2, buf); \n\
+  \  \n\
+  \    } else if(wireType == WireType.LengthDelim) { \n\
+  \      throw; // Not implemented \n\
+  \  \n\
+  \    } else if(wireType == WireType.StartGroup) { \n\
+  \      throw; // Deprecated, not implemented\n\
+  \  \n\
+  \    } else if(wireType == WireType.EndGroup) { \n\
+  \      throw; // Deprecated, not implemented \n\
+  \  \n\
+  \    } else if(wireType == WireType.Fixed32) { \n\
+  \      uint32 x3 = readUInt32(ptr, data); \n\
+  \      ptr += 4; \n\
+  \      setField_uint32(fieldId, x3, buf); \n\
+  \    } \n\
+  \  } \n\
+  \  return buf; \n\
+  \}" t t t
+
+staticCode :: String
+staticCode = "\
+  \enum WireType { Varint, Fixed64, LengthDelim, StartGroup, EndGroup, Fixed32 } \n\
+  \\n\
+  \function readKey(uint ptr, bytes data) constant returns (uint, WireType, uint) { \n\
+  \  var (x, n) = readVarInt(ptr, data); \n\
+  \  WireType typeId  = WireType(x & 7); \n\
+  \  uint fieldId = x >> 3; \n\
+  \  return (fieldId, typeId, n); \n\
+  \} \n\
+  \\n\
+  \function readVarInt(uint ptr, bytes data) constant returns (uint, uint) { \n\
+  \  return (0, ptr); \n\
+  \} \n\
+  \\n\
+  \function readUInt32(uint ptr, bytes data) constant returns (uint32) { \n\
+  \  return 0; \n\
+  \} \n\
+  \\n\
+  \function readUInt64(uint ptr, bytes data) constant returns (uint64) { \n\
+  \  return 0; \n\
+  \} \n\
+  \"
+
