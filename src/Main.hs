c84e5e9f326b230e4be32edc7989709b2eed3d7a
Luke Williams <shmookey@shmookey.net>
2017-03-10 14:39:00 +1100

    Add option to produce solidity version pragmas

--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,7 @@ import Data.List (intercalate)
 data Options = Options
   { optDir    :: FilePath
   , optSuffix :: String
+  , optPragma :: Bool
   , optInput  :: String
   } deriving (Show)
 
@@ -28,20 +29,25 @@ readCliOpts =
           ( Opts.long    "out"
          <> Opts.short   'o'
          <> Opts.value   "."
-         <> Opts.help    "Output directory." )
+         <> Opts.help    "Output directory" )
       <*> Opts.strOption
           ( Opts.long    "suffix"
          <> Opts.short   's'
          <> Opts.value   "_pb"
          <> Opts.help    "Output file name suffix" )
+      <*> Opts.switch
+          ( Opts.long    "pragma"
+         <> Opts.short   'p'
+         <> Opts.help    "Emit solidity version pragmas in generated code" )
       <*> Opts.argument Opts.str
           ( Opts.metavar "FILE"
-         <> Opts.help    "Path to input file." )
+         <> Opts.help    "Path to input file" )
 
-processDescriptor :: FilePath -> String -> Proto.FileDescriptorProto -> IO ()
-processDescriptor outDir suffix file =
+processDescriptor :: Bool -> FilePath -> String -> Proto.FileDescriptorProto -> IO ()
+processDescriptor pragma outDir suffix file =
   let
-    writeSrc (k, v) = writeFile (outDir ++ "/" ++ k ++ suffix ++ ".sol") v
+    header          = if pragma then "pragma solidity ^0.4.1;\n\n" else ""
+    writeSrc (k, v) = writeFile (outDir ++ "/" ++ k ++ suffix ++ ".sol") (header ++ v)
   in
     mapM_ writeSrc $ Convert.convert file
 
@@ -51,6 +57,7 @@ main = readCliOpts >>= \o ->
     input  = optInput o
     outDir = optDir o
     suffix = optSuffix o
+    pragma = optPragma o
   in do
     result <- Parser.parseProto input <$> B.readFile input
 
@@ -60,5 +67,5 @@ main = readCliOpts >>= \o ->
 
     case result of
       Left e  -> putStrLn $ show e
-      Right x -> processDescriptor outDir suffix x
+      Right x -> processDescriptor pragma outDir suffix x
 
