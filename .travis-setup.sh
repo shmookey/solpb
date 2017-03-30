919db0f4e43aae2fc544045a62df61bf49866259
Luke Williams <shmookey@shmookey.net>
2017-03-31 01:35:43 +1100

    No travis_retry in shell scripts

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -6,7 +6,7 @@ mkdir -p $HOME/.local/bin
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]
 then
-  travis_retry curl --insecure -L https://www.stackage.org/stack/osx-x86_64
+  curl --insecure -L https://www.stackage.org/stack/osx-x86_64
   tar xz --strip-components=1 --include '*/stack' -C $HOME/.local/bin
   brew tap ethereum/ethereum
   brew install solidity
@@ -16,7 +16,7 @@ fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]] 
 then
   gem install --no-ri --no-rdoc fpm
-  travis_retry curl -L https://www.stackage.org/stack/linux-x86_64
+  curl -L https://www.stackage.org/stack/linux-x86_64
   tar xz --wildcards --strip-components=1 -C $HOME/.local/bin '*/stack'
   curl $DEPS_URL/$TRAVIS_OS_NAME/solc -o $HOME/.local/bin/solc
   chmod +x $HOME/.local/bin/solc
